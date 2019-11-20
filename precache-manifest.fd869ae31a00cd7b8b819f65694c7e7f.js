0048aad5165f5409e8556d91d239293c7a5478ee
tawat chai <tawat1838@gmail.com>
2019-11-20 10:45:18 +0700

    Updates

---
+++ precache-manifest.fd869ae31a00cd7b8b819f65694c7e7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9762caf659869709847323055840eb4c",
+    "url": "/FFC-REPORT/index.html"
+  },
+  {
+    "revision": "55928a6ba72b96a05ef8",
+    "url": "/FFC-REPORT/static/css/2.e875816a.chunk.css"
+  },
+  {
+    "revision": "1974f62486013f72b8b8",
+    "url": "/FFC-REPORT/static/css/3.29814d70.chunk.css"
+  },
+  {
+    "revision": "4f8cc3343368d47a872a",
+    "url": "/FFC-REPORT/static/css/main.d24d6573.chunk.css"
+  },
+  {
+    "revision": "55928a6ba72b96a05ef8",
+    "url": "/FFC-REPORT/static/js/2.bf9cb9de.chunk.js"
+  },
+  {
+    "revision": "1974f62486013f72b8b8",
+    "url": "/FFC-REPORT/static/js/3.29985498.chunk.js"
+  },
+  {
+    "revision": "f39d7c7c1f76683a2fa3",
+    "url": "/FFC-REPORT/static/js/4.9199579d.chunk.js"
+  },
+  {
+    "revision": "4f8cc3343368d47a872a",
+    "url": "/FFC-REPORT/static/js/main.c980f879.chunk.js"
+  },
+  {
+    "revision": "01168490df479c6f8cb7",
+    "url": "/FFC-REPORT/static/js/runtime~main.f183f7b6.js"
+  }
+]);
